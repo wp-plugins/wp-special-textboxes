cd8979e2df5b8f7335540ee6d604c3720f0448a5
minimus <minimus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-12 16:32:52 +0000

    tagging version 3.10.59
    git-svn-id: https://plugins.svn.wordpress.org/wp-special-textboxes/tags/3.10.59@359209 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/wstb-dialog.js
+++ js/wstb-dialog.js
@@ -16,38 +16,49 @@ function insertWSTBCode() {
 	
 	var wstbCode;
 	
-	var wstbIDObj = document.getElementById('wstb_id');
-	var wstbID = wstbIDObj.value;
-	var wstbCaptionObj = document.getElementById('wstb_caption');
-	var wstbCaption = wstbCaptionObj.value;
-	var wstbFloatObj = document.getElementById('wstb_float');
-	var wstbFloat = wstbFloatObj.checked;
-	var wstbAlignObj = document.getElementById('wstb_align');
-	var wstbAlign = wstbAlignObj.value;
-	var wstbWidthObj = document.getElementById('wstb_width');
-	var wstbWidth = wstbWidthObj.value;
-	var wstbColorObj = document.getElementById('wstb_fcolor');
-	var wstbColor = wstbColorObj.value.replace("#", "");
-	var wstbCColorObj = document.getElementById('wstb_cfcolor');
-	var wstbCColor = wstbCColorObj.value.replace("#", "");
-	var wstbBGColorObj = document.getElementById('wstb_bgcolor');
-	var wstbBGColor = wstbBGColorObj.value.replace("#", "");
-	var wstbCBGColorObj = document.getElementById('wstb_cbgcolor');
-	var wstbCBGColor = wstbCBGColorObj.value.replace("#", "");
-	var wstbBColorObj = document.getElementById('wstb_bcolor');
-	var wstbBColor = wstbBColorObj.value.replace("#", "");
-	var wstbImageObj = document.getElementById('wstb_image_url');
-	var wstbImage = wstbImageObj.value;
-	var wstbBigImageObj = document.getElementById('wstb_big_image');
-	var wstbBigImage = wstbBigImageObj.checked;
-	var wstbNoImageObj = document.getElementById('wstb_noimage');
-	var wstbNoImage = wstbNoImageObj.checked;
+	var f = document.forms[0];
+	var radio = f.elements.wstb_collapsed;
+  var cRadio = f.elements.wstb_collapsing;
+	
+	var wstbID = f.elements.wstb_id.value;
+	var wstbCaption = f.elements.wstb_caption.value;
+	var wstbFloat = f.elements.wstb_float.checked;
+	var wstbAlign = f.elements.wstb_align.value;
+	var wstbWidth = f.elements.wstb_width.value;
+	var wstbColor = f.elements.wstb_fcolor.value.replace("#", "");
+	var wstbCColor = f.elements.wstb_cfcolor.value.replace("#", "");
+	var wstbBGColor = f.elements.wstb_bgcolor.value.replace("#", "");
+	var wstbCBGColor = f.elements.wstb_cbgcolor.value.replace("#", "");
+	var wstbBColor = f.elements.wstb_bcolor.value.replace("#", "");
+	var wstbImage = f.elements.wstb_image_url.value;
+	var wstbBigImage = f.elements.wstb_big_image.checked;
+	var wstbNoImage = f.elements.wstb_noimage.checked;
+  var wstbLeftMargin = f.elements.wstb_left_margin.value;
+  var wstbRightMargin = f.elements.wstb_right_margin.value;
+  var wstbTopMargin = f.elements.wstb_top_margin.value;
+  var wstbBottomMargin = f.elements.wstb_bottom_margin.value;
+  var wstbCollapsing = 0;
+	var wstbCollapsed = 0;
+  
+	if(cRadio[0].checked) wstbCollapsing = 1;
+  else if(cRadio[1].checked) wstbCollapsing = 2;
+  
+  if(radio[0].checked) wstbCollapsed = 1;
+	else if(radio[1].checked) wstbCollapsed = 2;
 	
 	var contentObj = tinyMCE.getInstanceById('content');
 	var wstbBody = contentObj.selection.getContent();
 	
 	wstbCode = ' [stextbox id="' + wstbID + '"'; 
-	if (wstbCaption != '') wstbCode += ' caption="' + wstbCaption + '"';
+	if (wstbCaption != '') { 
+		wstbCode += ' caption="' + wstbCaption + '"';
+		
+    if (wstbCollapsing == 1) wstbCode += ' collapsing="true"';
+    else if(wstbCollapsing == 2) wstbCode += ' collapsing="false"';
+    
+    if ((wstbCollapsed == 1) && (wstbCollapsing != 2)) wstbCode += ' collapsed="true"';
+		else if(wstbCollapsed == 2) wstbCode += ' collapsed="false"';
+	}
 	if (wstbFloat) {
 		wstbCode += ' float="true"';
 		if (wstbAlign != 'left') wstbCode += ' align="right"';
@@ -58,6 +69,10 @@ function insertWSTBCode() {
 	if (wstbBColor != '') wstbCode += ' bcolor="' + wstbBColor + '"';
 	if (wstbBGColor != '') wstbCode += ' bgcolor="' + wstbBGColor + '"';
 	if (wstbCBGColor != '') wstbCode += ' cbgcolor="' + wstbCBGColor + '"';
+  if (wstbLeftMargin != '') wstbCode += ' mleft="' + wstbLeftMargin + '"';
+  if (wstbRightMargin != '') wstbCode += ' mright="' + wstbRightMargin + '"';
+  if (wstbTopMargin != '') wstbCode += ' mtop="' + wstbTopMargin + '"';
+  if (wstbBottomMargin != '') wstbCode += ' mbottom="' + wstbBottomMargin + '"';
 	if ((wstbImage != '') & !wstbNoImage) wstbCode += ' image="' + wstbImage + '"';
 	if (wstbBigImage) wstbCode += ' big="' + wstbBigImage.toString() + '"';
 	if (wstbNoImage) wstbCode += ' image="null"';
