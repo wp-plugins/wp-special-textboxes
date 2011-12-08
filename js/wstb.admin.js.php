e4b2aa4339004f791f21d1a3a74850f1d095fbdf
minimus <minimus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-08 17:35:53 +0000

    Tagging version 4.1.69
    git-svn-id: https://plugins.svn.wordpress.org/wp-special-textboxes/tags/4.1.69@472531 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/wstb.admin.js.php
+++ js/wstb.admin.js.php
@@ -11,29 +11,32 @@ $stextboxesOptions = $stbObject->getAdminOptions();
 
 ?>
 
-jQuery(document).ready(function($){
-	var options = {};
+(function($){
+	$(document).ready(function() {
+		var options = {direction: 'vertical'};
 	
-	function callback(sb,sc,si) {
-		if (sb.css('display') == 'none') {
-			sc.css({'margin-bottom' : '<?php echo $stextboxesOptions['bottom_margin']; ?>px'});
-			si.attr({'src' : '<?php echo WP_PLUGIN_URL.'/wp-special-textboxes/images/show.png';?>', 'title' : '<?php _e('Show', 'wp-special-textboxes');?>'});
-			<?php if ($stextboxesOptions['rounded_corners'] === 'true') { ?>
-			sc.css({'-webkit-border-bottom-left-radius' : '5px', 
-							'-webkit-border-bottom-right-radius' : '5px', 
-							'-moz-border-radius-bottomleft' : '5px', 
-							'-moz-border-radius-bottomright' : '5px'});
-			<?php }?>
-		}
-		else {
-			si.attr({'src' : '<?php echo WP_PLUGIN_URL.'/wp-special-textboxes/images/hide.png';?>', 'title' : '<?php _e('Hide', 'wp-special-textboxes');?>'});
-		}
+		function callback(sb,sc,si) {
+			if (sb.css('display') == 'none') {
+				sc.css({'margin-bottom' : '<?php echo $stextboxesOptions['bottom_margin']; ?>px'});
+				si.attr({'src' : '<?php echo WP_PLUGIN_URL.'/wp-special-textboxes/images/show.png';?>', 'title' : '<?php _e('Show', 'wp-special-textboxes');?>'});
+				<?php if ($stextboxesOptions['rounded_corners'] === 'true') { ?>
+				sc.css({'-webkit-border-bottom-left-radius' : '5px', 
+								'-webkit-border-bottom-right-radius' : '5px', 
+								'-moz-border-radius-bottomleft' : '5px', 
+								'-moz-border-radius-bottomright' : '5px',
+                'border-bottom-left-radius' : '5px', 
+								'border-bottom-right-radius' : '5px'});
+				<?php }?>
+			}
+			else {
+				si.attr({'src' : '<?php echo WP_PLUGIN_URL.'/wp-special-textboxes/images/hide.png';?>', 'title' : '<?php _e('Hide', 'wp-special-textboxes');?>'});
+			}
 		
-		$(this).parent().parent().children('#caption').css({'margin-bottom' : '<?php echo $stextboxesOptions['bottom_margin']; ?>px'});
-		return false;
-	}
+			$(this).parent().parent().children('#caption').css({'margin-bottom' : '<?php echo $stextboxesOptions['bottom_margin']; ?>px'});
+			return false;
+		}
 	
-	$(".stb-tool").bind("click", function() {
+		$(".stb-tool").bind("click", function() {
 			sb = $(this).parent().parent().children('#body');
 			sc = $(this).parent().parent().children('#caption');
 			si = $(this).children('#stb-toolimg');
@@ -48,22 +51,27 @@ jQuery(document).ready(function($){
 				sc.css({'-webkit-border-bottom-left-radius' : '0px', 
 								'-webkit-border-bottom-right-radius' : '0px', 
 								'-moz-border-radius-bottomleft' : '0px', 
-								'-moz-border-radius-bottomright' : '0px'});
+								'-moz-border-radius-bottomright' : '0px',
+                'border-bottom-left-radius' : '0px', 
+								'border-bottom-right-radius' : '0px'});
 				<?php }?>
 			}
 			return false;
-	});
+		});
+    
+    $("#tabs").tabs();
 	
-	$('#cb_color, #cb_caption_color, #cb_background, #cb_caption_background, #cb_border_color').ColorPicker({
-  		onSubmit: function(hsb, hex, rgb, el){
-  			$(el).val(hex);
-  			$(el).ColorPickerHide();
-  		},
-  		onBeforeShow: function(){
-  			$(this).ColorPickerSetColor(this.value);
-  		}
-  	}).bind('keyup', function(){
+		$('#cb_color, #cb_caption_color, #cb_background, #cb_caption_background, #cb_border_color').ColorPicker({
+  			onSubmit: function(hsb, hex, rgb, el){
+  				$(el).val(hex);
+  				$(el).ColorPickerHide();
+  			},
+  			onBeforeShow: function(){
+  				$(this).ColorPickerSetColor(this.value);
+  			}
+  		}).bind('keyup', function(){
   		$(this).ColorPickerSetColor(this.value);
   	});
-	
+		return false;
+	});
 })(jQuery)
