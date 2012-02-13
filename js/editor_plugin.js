3a903cb37ac4c79d34a3824ad0f3cb40095e63c9
minimus <minimus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-13 20:09:11 +0000

    Tagging Version 4.3.72
    git-svn-id: https://plugins.svn.wordpress.org/wp-special-textboxes/tags/4.3.72@504454 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/editor_plugin.js
+++ js/editor_plugin.js
@@ -14,8 +14,8 @@
 					
 				ed.windowManager.open({
 					file : url + '/dialog.php',
-					width : 450 + parseInt(ed.getLang('wstb.delta_width', 0)),
-					height : 230 + parseInt(ed.getLang('wstb.delta_height', 0)),
+					width : 520 + parseInt(ed.getLang('wstb.delta_width', 0)),
+					height : 475 + parseInt(ed.getLang('wstb.delta_height', 0)),
 					inline : 1
 				}, {
 					plugin_url : url 
@@ -40,9 +40,9 @@
 			return {
 					longname  : 'Special Text Boxes',
 					author 	  : 'minimus',
-					authorurl : 'http://blogovod.co.cc',
-					infourl   : 'http://blogovod.co.cc',
-					version   : "3.1.29"
+					authorurl : 'http://blogcoding.ru',
+					infourl   : 'http://www.simplelib.com',
+					version   : "4.2.70"
 			};
 		}
 	});
